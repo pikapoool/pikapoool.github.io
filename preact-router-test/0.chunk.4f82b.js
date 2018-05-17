260278a058f57e1e3a080eaae73a1131b0a91164
Oleksandr Ilchenko <ilchenko.sasha@gmail.com>
2018-05-17 13:28:31 +0300

    preact-router-test

---
+++ preact-router-test/0.chunk.4f82b.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{c3ny:function(e,n,t){"use strict";function c(e){t.e(5).then(function(){e(t("B9hw"))}.bind(null,t)).catch(t.oe)}Object.defineProperty(n,"__esModule",{value:!0});var u=t("7N8r");n.default=t.n(u)()(c)}});
+//# sourceMappingURL=0.chunk.4f82b.js.map
---
+++ preact-router-test/1.chunk.395f6.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{P43e:function(e,n,t){"use strict";function c(e){t.e(6).then(function(){e(t("iOg+"))}.bind(null,t)).catch(t.oe)}Object.defineProperty(n,"__esModule",{value:!0});var u=t("7N8r");n.default=t.n(u)()(c)}});
+//# sourceMappingURL=1.chunk.395f6.js.map
