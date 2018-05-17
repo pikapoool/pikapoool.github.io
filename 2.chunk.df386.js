901ade4998223d21b3bf31d10acfa14d31653a62
Oleksandr Ilchenko <ilchenko.sasha@gmail.com>
2018-05-17 13:44:40 +0300

    remove preact-router-test

--- 2.chunk.df386.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{"927P":function(e,n,t){"use strict";function u(e){t.e(7).then(function(){e(t("OuSm"))}.bind(null,t)).catch(t.oe)}Object.defineProperty(n,"__esModule",{value:!0});var c=t("7N8r");n.default=t.n(c)()(u)}});
-//# sourceMappingURL=2.chunk.df386.js.map
