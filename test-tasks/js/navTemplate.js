5fa5d2f9d174d613371f84694e5f43db5bce1447
oleksandr Ilchenko <oleksandr.ilchenko@materialise.kiev.ua>
2022-08-18 20:25:46 +0200

    add symbol examples

---
+++ test-tasks/js/navTemplate.js
@@ -0,0 +1,32 @@
+function createDOM(htmlText) {
+  return new DOMParser().parseFromString(htmlText, "text/html");
+}
+
+function createTemplate(templateText) {
+  const dom = createDOM(templateText);
+  const template = dom.querySelector("template");
+  return template;
+}
+
+const template = createTemplate(`
+  <template>
+    <div>
+      <a href="./index.html">Home</a><span> | </span>
+      <a href="./promises.html">Promises</a><span> | </span>
+      <a href="./symbols.html">Symbol</a><span> | </span>
+      <a href="./VirtualDom.html">VirtualDom</a><span> | </span>
+      <a href="https://dou.ua/lenta/articles/interview-questions-javascript-developer/" target="_blank">300+ questions for Junior, Middle, Senior</a>
+    </div>
+  </template>
+`);
+
+class CustomHTMLElement extends HTMLElement {
+  constructor() {
+    super();
+    const templateContent = template.content;
+    const shadowRoot = this.attachShadow({ mode: "closed" });
+    shadowRoot.appendChild(templateContent.cloneNode(true));
+  }
+}
+
+customElements.define("nav-element", CustomHTMLElement);
