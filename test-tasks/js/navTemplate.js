4d147c6d1b07af5a1671b5a4c729955980788579
oleksandr Ilchenko <oleksandr.ilchenko@materialise.kiev.ua>
2022-11-16 12:16:50 +0100

    rxjs quiz

--- test-tasks/js/navTemplate.js
+++ test-tasks/js/navTemplate.js
@@ -16,6 +16,8 @@ const template = createTemplate(`
       <a href="./symbols.html">Symbol</a><span> | </span>
       <a href="./VirtualDom.html">VirtualDom</a><span> | </span>
       <a href="./angular.html">Angular</a><span> | </span>
+      <a href="./typescript.html">Typescript</a><span> | </span>
+      <a href="./rxjs.html">RxJs</a><span> | </span>
       <a href="./node.html">NodeJs</a><span> | </span>
       <a href="https://dou.ua/lenta/articles/interview-questions-javascript-developer/" target="_blank">300+ questions for Junior, Middle, Senior</a>
     </div>
